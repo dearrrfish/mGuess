6eaed10788d251bd9974a3038ff0b4a58b124d3c
Yu Jin <dearrrfish@gmail.com>
2012-04-16 22:07:23 -0700

    Initial Commit

---
+++ mGuess/mGuessAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  mGuessAppDelegate.h
+//  mGuess
+//
+//  Created by Yu Jin on 4/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class mGuessViewController;
+
+@interface mGuessAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) mGuessViewController *viewController;
+
+@end
---
+++ mGuess/mGuessViewController.h
@@ -0,0 +1,13 @@
+//
+//  mGuessViewController.h
+//  mGuess
+//
+//  Created by Yu Jin on 4/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mGuessViewController : UIViewController
+
+@end
